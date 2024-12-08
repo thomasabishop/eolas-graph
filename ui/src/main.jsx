10aad035f6346907ee32babcc33c33c5dce08a4a
thomasabishop <tactonbishop@gmail.com>
2024-12-08 13:09:02 +0000

    feat (ui): strip react starter to basic files

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// const domNode = document.getElementById("eolas-graph")
+// const root = createRoot(domNode)
+
+// root.render(<App />)
+
+function initializeReactApp() {
+   const domNode = document.getElementById("eolas-graph")
+   if (!domNode) {
+      console.error("Target container #eolas-graph not found")
+      return
+   }
+   const root = createRoot(domNode)
+   root.render(<App />)
+}
+
+document.addEventListener("DOMContentLoaded", initializeReactApp)
