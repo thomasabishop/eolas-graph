635d6d8d481b6c1f3a5a2a0d474c1ba02d3d70f2
thomasabishop <tactonbishop@gmail.com>
2024-12-08 15:49:33 +0000

    chore (ui): tidy up css files

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-// import "./App.css"
+import "./App.css"
 
 function App() {
-	return (
-		<React.Fragment>
-			<p className="test">This is my application</p>
-		</React.Fragment>
-	)
+   return (
+      <React.Fragment>
+         <p className="test">This is my application</p>
+      </React.Fragment>
+   )
 }
 
 export default App
--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,20 +1,14 @@
 import { createRoot } from "react-dom/client"
-import "./index.css"
 import App from "./App.jsx"
 
-// const domNode = document.getElementById("eolas-graph")
-// const root = createRoot(domNode)
-
-// root.render(<App />)
-
 function initializeReactApp() {
-   const domNode = document.getElementById("eolas-graph")
-   if (!domNode) {
-      console.error("Target container #eolas-graph not found")
-      return
-   }
-   const root = createRoot(domNode)
-   root.render(<App />)
+	const domNode = document.getElementById("eolas-graph")
+	if (!domNode) {
+		console.error("Target container #eolas-graph not found")
+		return
+	}
+	const root = createRoot(domNode)
+	root.render(<App />)
 }
 
 document.addEventListener("DOMContentLoaded", initializeReactApp)
